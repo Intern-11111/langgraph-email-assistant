2c7b010916281fe3297add333d8325942c10137e
payal <payalkokane19@gmail.com>
2026-01-17 19:25:36 +0530

    main1.py

--- main1.py
+++ main1.py
@@ -1,6 +1,5 @@
 from graph import email_agent_graph
-from src.HITL.hitl_graph import build_graph
-from langgraph.checkpoint.memory import MemorySaver
+
 """
 Here we run the agent with one email example and see how agent is works and make decision
 """
@@ -14,16 +13,11 @@ if __name__ == "__main__":
         }
     }
 
-    # final_state = email_agent_graph.invoke(state)
+    final_state = email_agent_graph.invoke(state)
 
-    # print("\nFINAL STATE:")
-    # for k, v in final_state.items():
-    #     print(f"{k}: {v}")
+    print("\nFINAL STATE:")
+    for k, v in final_state.items():
+        print(f"{k}: {v}")
 
-graph=build_graph()
-graph.compile(
-        checkpointer=MemorySaver(),
-        interrupt_after=["tool"]  
-    )
 
 
